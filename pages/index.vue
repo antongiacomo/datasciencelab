036ab092b74244c5b1a5ccea3f8578834fec95c3
Antongiacomo Polimeno <antongiacomo.polimeno@unimi.it>
2023-12-19 00:24:28 +0100

    errors?

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@ import { ClockIcon, CalendarIcon } from "@heroicons/vue/24/outline";
 import ArticleCard from "~/components/ArticleCard.vue";
 import Article from "~/components/Article.vue";
 // get all the articles inside the content folder
-var articles = await  queryContent("seminars").sort({ createdAt: 1 }).find();
+var {data: articles} = await useAsyncData( () => queryContent("seminars").sort({ createdAt: 1 }).find());
 
-var articlesPast = articles
+var articlesPast = articles.value
   .filter((article) => isPast(article.date))
   .reduce((grouped, object) => {
     const { date } = object;
@@ -17,7 +17,7 @@ var articlesPast = articles
     return grouped;
   }, {});
 
-var articlesFuture = articles.filter((article) => true /*!isPast(article.date)*/);
+var articlesFuture = articles.value.filter((article) => true /*!isPast(article.date)*/);
 
 function monthName(date) {
   return moment(date, "DD-MM-YYYY").format("MMMM YYYY");
